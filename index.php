1b2e8f5e59213e37b1e389f3031087c2be7ae070
Curtis Oakley <coakley@chocklydigital.com>
2013-09-13 14:46:32 -0600

    Modified the bootstrap to an initializer that can load the ban manager into any page.

--- index.php
+++ index.php
@@ -3,18 +3,11 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
         <title>Final Score MC Ban Manager</title>
-        <!-- Final Score Themeing -->
-<link rel='stylesheet' id='admin-bar-css'  href='http://finalscoremc.com/wp-includes/css/admin-bar.min.css?ver=3.5.1' type='text/css' media='all' />
-<link rel='stylesheet' id='bp-admin-bar-css'  href='http://finalscoremc.com/wp-content/plugins/buddypress/bp-core/css/admin-bar.css?ver=1.6.4' type='text/css' media='all' />
-<link rel='stylesheet' id='bbp-default-bbpress-css'  href='http://finalscoremc.com/wp-content/plugins/bbpress/templates/default/css/bbpress.css?ver=2.2.4' type='text/css' media='screen' />
-<link rel='stylesheet' id='bp-default-main-css'  href='http://finalscoremc.com/wp-content/plugins/buddypress/bp-themes/bp-default/_inc/css/default.css?ver=1.6.4' type='text/css' media='all' />
-<link rel='stylesheet' id='bp-default-responsive-css'  href='http://finalscoremc.com/wp-content/plugins/buddypress/bp-themes/bp-default/_inc/css/responsive.css?ver=1.6.4' type='text/css' media='all' />
-<style type="text/css">
-        body { background-color: #fdfeb8; background-image: url('http://finalscoremc.com/wp-content/uploads/2012/04/fsbackground.png'); background-repeat: repeat-x; background-position: top left; background-attachment: scroll; }
-</style>
-        <!-- END -->
+        <link href="css/custom-theme/jquery-ui-1.10.2.custom.min.css" type="text/css" rel="stylsheet" />
+        <link href="css/main.min.css" type="text/css" rel="stylesheet" />
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" ></script>
-        <script type="text/javascript" src="bm-bootstrap.js"></script>
+        <script type="text/javascript" src="jquery-ui-1.10.2.custom.min.js"></script>
+        <script type="text/javascript" src="ban-manager.js"></script>
     </head>
     <body class="page">
         <div id="container">
