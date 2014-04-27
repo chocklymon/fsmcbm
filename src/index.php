f1bd4bce11d9725ba84ce708985a1a67ccaab68a
Curtis Oakley <coakley@chocklydigital.com>
2014-04-27 10:20:32 -0600

    Experiment with using AngularJS for the presentation.

--- src/index.php
+++ src/index.php
@@ -5,84 +5,23 @@
         <title>Final Score MC Ban Manager</title>
         <link href="css/custom-theme/jquery-ui-1.10.2.custom.css" type="text/css" rel="stylesheet" />
         <link href="css/main.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container" data-ng-app="banManager">
+            <div class="bm-header ui-tabs">
+                <ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
+                    <li><a href="#/user">Manage</a></li>
+                    <li><a href="#/bans">Bans</a></li>
+                    <li><a href="#/watchlist">Watch-List</a></li>
+                    <li><a href="#/search">Search</a></li>
+                </ul>
+            </div>
+            <div data-ng-view></div>
+        </div>
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" ></script>
         <script type="text/javascript" src="js/jquery-ui-1.10.2.custom.min.js"></script>
-        <script type="text/javascript" src="js/bm-config.js.php"></script>
-        <script type="text/javascript" src="js/ban-manager.js"></script>
-    </head>
-    <body class="page">
-        <div id="container">
-            <div id="content">
-                <div class="left-menu">
-                    <div id="search-box" class="form">
-                        <label for="lookup">Lookup:</label>
-                        <input type="text" id="lookup" />
-                        <input type='hidden' id="lookup-user_id" />
-                        <br/>
-                        <label for="search">Search:</label>
-                        <input type="text" id="search" />
-                        <button id="search-button">Go</button>
-                    </div>
-                    <div id="actions">
-                        <button id="add-user">Add User</button>
-                        <button id="add-incident">Add Incident</button>
-                    </div>
-                </div>
-                <div id="tabs" class="main-column" style="overflow:auto"><!-- Overflow auto - fixes tabs height being weird with floats -->
-                    <ul>
-                        <li><a href="#manage">Manage</a></li>
-                        <li><a href="ban-manager.php?action=get_bans">Bans</a></li>
-                        <li><a href="ban-manager.php?action=get_watchlist">Watch-List</a></li>
-                        <li><a href="#search-tab">Search</a></li>
-                    </ul>
-                    <div id="manage">
-                        <div id="user-info" class="form">
-                            <h5>Select a user to manage.</h5>
-                        </div>
-                        <div id="incident-info">
-
-                        </div>
-                        <div id="ban-history">
-
-                        </div>
-                    </div>
-                    <div id="search-tab">
-
-                    </div>
-                </div>
-                <!-- Message Boxes -->
-                <div id="error" class="ui-widget">
-                    <div class="ui-state-error ui-corner-all">
-                        <p>
-                            <span class="ui-icon ui-icon-info"></span>
-                            <strong>Error: </strong><span id="error-msg"></span>
-                        </p>
-                    </div>
-                </div>
-                <div id="highlight" class="ui-widget">
-                    <div class="ui-state-highlight ui-corner-all">
-                        <p>
-                            <span class="ui-icon ui-icon-alert"></span>
-                            <span id="highlight-msg"></span>
-                        </p>
-                    </div>
-                </div>
-                <!-- Dialogs -->
-                <div id="dialog-add-user" title="Add User">
-                    <form id="add-user-form" class="form">
-                        <label for="user_add_username">Username:</label>
-                        <input type="text" value="" id="user_add_username" name="username">
-                        <br/>
-                    </form>
-                </div>
-                <div id="dialog-add-incident" title="Add Incident">
-                    <form class="form" id="add-incident-form">
-                        <label for="user">Username:</label>
-                        <input type="text" id="user_name" />
-                        <input type='hidden' id="user_id" name="user_id" />
-                        <br/>
-                </div>
-            </div>
-        </div>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min.js"></script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-route.min.js"></script>
+        <script type="text/javascript" src="presentation/assets/js/app.js"></script>
     </body>
 </html>
---
+++ src/presentation/assets/js/app.js
@@ -0,0 +1,87 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2014 Curtis Oakley.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+angular.module('banManager', ['ngRoute']).filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+}).config(['$routeProvider', function($routeProvider) {
+    // Set up the routes
+    $routeProvider
+        .when('/', {
+            controller:  'user',
+            templateUrl: 'presentation/views/user.html'
+        })
+        .when('/user/:username', {
+                controller:  'user',
+                templateUrl: 'presentation/views/user.html'
+            })
+        .when('/bans', {
+                controller:  'userList',
+                templateUrl: 'presentation/views/userlist.html'
+            })
+        .when('/watchlist', {
+                controller:  'userList',
+                templateUrl: 'presentation/views/userlist.html'
+            })
+        .when('/search', {
+                controller:  'search',
+                templateUrl: 'presentation/views/search.html'
+            })
+        .otherwise({ redirectTo: '/' });
+}]).controller('user', ['$scope', function($scope) {
+    // TODO
+    $scope.incidents = [{
+        id:5,
+        moderator:'Nefyoni',
+        dateCreated:'2013-05-29',
+        dateModified:'2013-07-29',
+        type:'destruction',
+        date:'2013-05-28',
+        notes:'Something bad happened',
+        actionTaken:'Repaired the damage'
+    }];
+    $scope.history = [{
+        moderator: 'fredwaffles',
+        date: '2013-06-06 14:23:50',
+        banned: true,
+        permanent: false
+    }];
+}]).controller('userList', ['$scope', '$location', '$http', function($scope, $location, $http) {
+    var endpoint = $location.path() === '/bans' ? 'get_bans' : 'get_watchlist';
+    var parameters = {
+        nonce: Math.random(),
+        timestamp: new Date().toUTCString()
+    };
+    $http.get("ban-manager.php?action=" + endpoint, {params: parameters})
+        .success(function(data) {
+            console.log(data);
+            $scope.users = [{
+                username: 'greg',
+                incidentDate: '2014-04-04',
+                incidentType: 'Theft',
+                actionTaken: 'Banned'
+            }];
+        });
+}]);
+
