4e4768f71f4b95add4826a676a83f6dfd3738d37
Curtis Oakley <coakley@chocklydigital.com>
2015-03-05 10:37:47 -0700

    Use bower to manager the JS dependencies

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Final Score MC Ban Manager</title>
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="presentation/assets/css/loading-bar.min.css" />
+        <link rel="stylesheet" href="presentation/assets/vendor/angular-loading-bar/build/loading-bar.min.css" />
         <link rel="stylesheet" href="presentation/assets/css/base.css" />
     </head>
     <body>
@@ -15,10 +15,10 @@
                 Loading...
             </div>
         </div>
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min.js"></script>
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-route.min.js"></script>
-        <script type="text/javascript" src="presentation/assets/js/ui-bootstrap-tpls-0.12.0.min.js"></script>
-        <script type="text/javascript" src="presentation/assets/js/loading-bar.min.js"></script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.28/angular.min.js"></script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.28/angular-route.min.js"></script>
+        <script type="text/javascript" src="presentation/assets/vendor/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>
+        <script type="text/javascript" src="presentation/assets/vendor/angular-loading-bar/build/loading-bar.min.js"></script>
         <script type="text/javascript" src="presentation/assets/js/app.js"></script>
     </body>
 </html>
